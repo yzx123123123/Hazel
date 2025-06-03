51355cbc41aad3364f31a83a3048048e2ecdf08a
yzx123123123 <2544170526@qq.com>
2025-06-03 20:06:46 +0800

    添加数学库glm

--- premake5.lua
+++ premake5.lua
@@ -16,10 +16,12 @@ IncludeDir ={}
 IncludeDir["GLFW"] ="Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] ="Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] ="Hazel/vendor/imgui"
+IncludeDir["glm"] ="Hazel/vendor/glm"
 
 include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
 include "Hazel/vendor/imgui"
+
 
 
 
@@ -38,7 +40,9 @@ project "Hazel"
      files
      {
          "%{prj.name}/src/**.h",
-         "%{prj.name}/src/**.cpp"
+         "%{prj.name}/src/**.cpp",
+         "%{prj.name}/vendor/glm/glm/**.hpp",
+		 "%{prj.name}/vendor/glm/glm/**.inl",
      }
 
      includedirs
@@ -47,7 +51,8 @@ project "Hazel"
          "%{prj.name}/vendor/spdlog/include",
          "%{IncludeDir.GLFW}",
          "%{IncludeDir.Glad}",
-         "%{IncludeDir.ImGui}"
+         "%{IncludeDir.ImGui}",
+         "%{IncludeDir.glm}"
      }
 
      links{
@@ -117,7 +122,8 @@ project "Sandbox"
      {
          "Hazel/vendor/spdlog/include",
          "Hazel/src",
-         "%{IncludeDir.ImGui}"
+         --"%{IncludeDir.ImGui}",
+         "%{IncludeDir.glm}"
 
      }
      
